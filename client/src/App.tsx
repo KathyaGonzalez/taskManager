391e3938d22200f724b8c3283e0b9e71a250cb4e
KathyaGonzalez <gonzco32@gmail.com>
2026-02-22 11:06:02 -0600

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Typography from "@mui/material/Typography";
+
+
+function App() {
+
+  return (
+    <>
+      <Typography variant="h4" component="h1" gutterBottom>
+          Gestor de Tareas Standard
+      </Typography>
+    </>
+  )
+}
+
+export default App
